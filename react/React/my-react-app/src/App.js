98841ae12bb26ce73201a1ad4df84bab21729041
Fjl98 <116802483+Fjl98@users.noreply.github.com>
2023-03-04 12:44:01 +0100

    4-m

--- react/React/my-react-app/src/App.js
+++ react/React/my-react-app/src/App.js
@@ -1,4 +1,43 @@
 
+import { useState } from "react";
+function App() {
+
+  const [infoContacto,setInfoContacto] = useState({
+    nombre: "",
+    email: "",
+    tlf:""
+  });
+
+  const manejaCambio = (evento) => {
+    setInfoContacto({...infoContacto,[evento.target.name]:evento.target.value});
+  }
+
+  const manejaSubmit = (evento) =>{
+    evento.preventDefault();
+    console.log(infoContacto);
+  }
+
+  return (
+    <div>
+      <form onSubmit={manejaSubmit}>
+        <h3>Contacto</h3>
+        <br></br>
+        <input type="text" name="nombre" id="nombre" onChange={manejaCambio}></input>
+        <br></br><br></br>
+        <input type="email" name="email" id="email" onChange={manejaCambio}></input>
+        <br></br><br></br>
+        <input type="number" name="tlf" id="tlf" onChange={manejaCambio}></input>
+        <br></br><br></br>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
 import React from "react";
 
 //USO DE ESTADOS 
@@ -30,14 +69,6 @@ function App() {
 
 
 export default App;
-
-
-
-
-
-
-
-
 
 
 
--- react/React/my-react-app/src/index.js
+++ react/React/my-react-app/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <App></App>
+);
+
+
+/*
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from "./App";
 
 
@@ -9,6 +21,104 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App></App>
 );
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//CREACION DE UN RELOJ
+let Reloj = () =>{
+  let [ahora,setAhora] = React.useState(new Date().toLocaleTimeString());
+  let elemento = (
+    <div className="container">
+      <span className="reloj">{ahora}</span>
+    </div>
+    
+    );
+
+  
+  root.render(elemento);
+}
+
+Reloj();
+
+setInterval(tictac,1000);
+
+*/
+
+  
+/*
+function tictac() {
+
+let ahora = new Date().toLocaleTimeString();
+
+  let elemento = (
+<div className="container">
+  <span className="reloj">{hora}</span>
+</div>
+);
+root.render(elemento);
+}
+
+
+tictac();
+
+setInterval(tictac,1000);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+let getPlaneta = () => "tierra";
+let numeros = [1, 2, 3, 4, 5];
+let elementos = numeros.map((numero) => <li>{numero}</li>);
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <div>
+     <p>
+     {elementos}
+     </p>
+   </div>
+ );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <p>hola{getPlaneta()}</p>
+  <App />
+  <App />
+  </div>
+);
+*/
 
 
 
