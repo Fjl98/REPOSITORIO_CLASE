609c623402a7c1eea0f9e64ad1aa7d2916514bed
fjl98 <jesuss.laguna@alumnos.cei.es>
2023-03-08 11:21:08 +0100

    8-m

--- react/React/my-react-app/src/Inicio-react/App8.js
+++ react/React/my-react-app/src/Inicio-react/App8.js
@@ -2,69 +2,94 @@
 // a una lista de cosas que hacer el usuario puede marcar
 //  como completado o no el elemento
 
-import { useState, useEffect, useRef, useReducer} from "react";
+import React, { useState, useReducer } from "react";
 
 const initialState = [
     {
-        id:1,
-        name:"terminar esta clase de reducer",
-        isCompleted: false,
-    },
-
+    id: 1,
+    name: "Terminar la clase de reducers",
+    isCompleted: false
+    }
 ];
 
-const reducer = (estado, accion)=>{
-    if (accion.type === "ADD_TODO") {
-        let name = accion.payload;
-        return[...estado,
-            {id : Math.ramdon(Math.floor())*9,name,isCompleted:false},
-        ];
+const reducer = (state, action) => {
+    if (action.type === "ADD_TODO") {
+    const { name } = action.payload;
+
+    return [
+        ...state,
+        {
+        id: Math.random()+1,
+        name,
+        isCompleted: false
+        }
+    ];
     }
-    if (accion.type === "DONE") {
-        let id = accion.payload; 
-        let newState = estado.map((unTodo) => {
-            if (unTodo.id == id) {
-                return {...unTodo,isCompleted:!unTodo.isCompleted};
-            }
-            return unTodo;
-        });
-        return newState;
+
+    if (action.type === "TOGGLE_IS_COMPLETED") {
+    const { id } = action.payload;
+
+    const newState = state.map((singleTodo) => {
+        if (singleTodo.id === id) {
+        return { ...singleTodo, isCompleted: !singleTodo.isCompleted };
+        }
+
+        return singleTodo;
+    });
+
+    return newState;
     }
+
+    return state;
 };
 
-function App(){
 
+function App() {
+    const [todoText, setTodoText] = useState("");
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    let [todoText,setTodoText]=useState("");
-    let[state,dispatch]= useReducer(reducer,initialState);
+    const handleChange = ({ target }) => setTodoText(target.value);
 
-    const manejaCambios = ({target})=>{
-        setTodoText(target.value);
+    const handleClick = () => {
+    dispatch({
+        type: "ADD_TODO",
+        payload: { name: todoText }
+    });
+    setTodoText("");
+    };
 
-    }
+    const handleToggle = (id) => {
+    dispatch({
+        type: "TOGGLE_IS_COMPLETED",
+        payload: { id }
+    });
+    };
 
-    const manejaAgregar = () => {
-        dispatch({type:"AD_TODO", payload:{name:todoText}});
-        setTodoText("");
-    }
-    return (<>
-    <p>Nuevo todo:
-        <input type="text" value={todoText} onChange={manejaCambios}></input>
-        <button onClick={manejaAgregar}>
-            Agregar
-        </button>
-    </p>
-    <ul>
-       {state.map(({name,isCompleted,id})=>{
-        let style= {
-            return(<li key={id} style={style} >{name}</li>);
-        });
-    }
-    </ul>
-    
-    </>);
-hola
+    return (
+    <>
+        <p>
+        Nuevo TODO:
+        <input type="text" value={todoText} onChange={handleChange} />
+        <button onClick={handleClick}>Agregar</button>
+        </p>
 
+        <h2>Listado</h2>
+
+        <ul>
+        {state.map(({ name, isCompleted, id }) => {
+            const style = {
+            textDecoration: isCompleted ? "line-through" : "inherit"
+            };
+
+            return (
+            <li key={id} style={style} onClick={() => handleToggle(id)}>
+                {name}
+            </li>
+            );
+        })}
+        </ul>
+    </>
+    );
 };
 
 export default App;
